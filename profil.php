caf836769d8b7f557b41d47decd41ace583105b6
yann-devriere <91664899+yann-devriere@users.noreply.github.com>
2022-01-10 09:22:22 +0100

    Update profil.php

--- profil.php
+++ profil.php
@@ -2,7 +2,7 @@
 session_start();
 	 
 	$db = new PDO('mysql:host=localhost;dbname=concoursChant', 'chant', '01021991');
-	$db = new PDO('mysql:host=localhost:3307;dbname=concoursChant;', 'chant' , '01021991');
+// 	$db = new PDO('mysql:host=localhost:3307;dbname=concoursChant;', 'chant' , '01021991');
 
 
 	?>
